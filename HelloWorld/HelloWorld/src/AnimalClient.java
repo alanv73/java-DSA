b25c28993e89efbb1aa6c1a084aff801548a01b3
Alan Van Art <alanv73@gmail.com>
2019-03-23 17:33:39 -0400

    initial commit from fawkes

---
+++ HelloWorld/HelloWorld/src/AnimalClient.java
@@ -0,0 +1,14 @@
+
+public class AnimalClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Create a new animal object
+		Animal myAnimal = new Animal();
+		myAnimal.name="tac";
+		System.out.println("Running AnimalClient.java");
+		System.out.println(myAnimal.name);
+	}
+
+}
