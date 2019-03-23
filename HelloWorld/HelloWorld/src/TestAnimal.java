6aac103eb3ad06716d72bc2acc63ab0cff84256e
Alan Van Art <alanv73@gmail.com>
2019-03-23 18:47:19 -0400

    new testAnimal

---
+++ HelloWorld/HelloWorld/src/TestAnimal.java
@@ -0,0 +1,14 @@
+
+public class TestAnimal extends Animal {
+
+	private String testAttribute;
+
+	public String getTestAttribute() {
+		return testAttribute;
+	}
+
+	public void setTestAttribute(String testAttribute) {
+		this.testAttribute = testAttribute;
+	}
+	
+}
