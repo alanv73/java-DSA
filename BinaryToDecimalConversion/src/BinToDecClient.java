e017c8a50d4e4eada6ee5d3044f8eb9e282b109e
Alan Van Art <alanv73@gmail.com>
2019-03-27 20:08:45 -0400

    code formatting

--- BinaryToDecimalConversion/src/BinToDecClient.java
+++ BinaryToDecimalConversion/src/BinToDecClient.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class BinToDecClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		BinaryToDecimal obj = new BinaryToDecimal();
+		
         Scanner scan=new Scanner(System.in);
-      System.out.println("Enter the binary number to convert into decimal");
-     int binaryNumber = scan.nextInt();
-    System.out.println( "Binary number " + binaryNumber + 
+        System.out.println("Enter the binary number to convert into decimal");
+        
+        int binaryNumber = scan.nextInt();
+        
+        System.out.println( "Binary number " + binaryNumber + 
     		" conversion in " + "Decimal number is " +
     		obj.BinToDec(binaryNumber));
 	}
--- BinaryToDecimalConversion/src/BinaryToDecimal.java
+++ BinaryToDecimalConversion/src/BinaryToDecimal.java
@@ -1,24 +1,21 @@
 
 public class BinaryToDecimal {
 
-	public int BinToDec(int binNumber)
-	{
-	   int decNumber= 0;
-	    int pw = 0;
-	 while(true)
-	       {
-	             if(binNumber == 0)
-	             {
-	                break;
-	             }
-	     else
-	       {
-	           int rem = binNumber%10;
-	           decNumber += rem*Math.pow(2, pw);
-	           binNumber = binNumber/10;
-	           pw++;
-	         }
-	     }
-	return decNumber;
+	public int BinToDec(int binNumber) {
+
+		int decNumber= 0;
+		int pw = 0;
+		while(true) {
+			if(binNumber == 0) {
+				break;
+        	}
+	   		else {	
+	   			int rem = binNumber%10;
+	   			decNumber += rem*Math.pow(2, pw);
+	   			binNumber = binNumber/10;
+	   			pw++;
+	   		}
+		}
+		return decNumber;
 	}
 }
